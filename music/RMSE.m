64f22291222edfcfac0ee6279caf2ca80334054b
Viviana Petrescu <petrescu.viviana@gmail.com>
2014-12-08 22:39:09 +0100

    BUG fix2 of RMSE

--- music/RMSE.m
+++ music/RMSE.m
@@ -5,6 +5,6 @@ function error = RMSE(Ypredicted, Ygroundtruth)
 
 nzindices = Ygroundtruth~= 0;
 temp = (Ypredicted(nzindices) - Ygroundtruth(nzindices)).^2;
-error =  sqrt(sum(temp(:))/ length(nzindices));
+error =  sqrt(sum(temp(:))/ nnz(Ygroundtruth));
 
 end
