77b98839122359bd8fbdc296c3603833cebc1bc9
Viviana Petrescu <petrescu.viviana@gmail.com>
2014-12-09 20:02:25 +0100

    added RecomExponentialALS

--- music/run_kfold_music.m
+++ music/run_kfold_music.m
@@ -1,17 +1,17 @@
 
 clear all;
 load songTrain;
-nbr_features = [20, 20, 50, 80, 100];
-alpha = [1,0.06, 0.1,1];
+nbr_features = [20];
+nlambda = [0.01, 0.1,0.5, 1];
 nbr_iterations = 1;
 K = 10;
-NF = 6;
+NF = 1;
 NL = 4;
 meanTrainRMSE = zeros(NF, NL,nbr_iterations,K);
 meanTestRMSE = zeros(NF, NL, nbr_iterations,K);
 
 for f_index = 1:NF
-  for a_index = 1: NL
+  for l_index = 1: NL
     for i = 1:nbr_iterations
         for kfold_iter = 1:K
             %% Do cross validation here
@@ -22,19 +22,18 @@ for f_index = 1:NF
             maxIters = 5;
             
             Ytrain_new(Ytrain_new~=0) = log(Ytrain_new(Ytrain_new~=0));
-            Ytrain_new(Ytrain_new >10) = 8;
             [U, A, train_error] = RecomExponentialALS(full(Ytrain_new), ...
                                            nbr_features(f_index),...
-                                           alpha(a_index), ...
+                                           nlambda(l_index), ...
                                            maxIters);
             Ypredicted = exp(U * A);
             test_error = RMSE(Ypredicted, Ytest_weak);
-            meanTrainRMSE(f_index, a_index, i, kfold_iter) = train_error;
-            meanTestRMSE(f_index, a_index, i, kfold_iter) = test_error;
+            meanTrainRMSE(f_index, l_index, i, kfold_iter) = train_error;
+            meanTestRMSE(f_index, l_index, i, kfold_iter) = test_error;
             fprintf('iterations %f %f\n', test_error, train_error)
         end
     end
   end
 end
 
-save('train_test_rmse.mat','meanTrainRMSE', 'meanTestRMSE')
+save('train_test_rmse_expALS_find_lambda.mat','meanTrainRMSE', 'meanTestRMSE')
